ab51b40efe515bc0b0c0f27396bee4710e431663
Zachary Briggs <zacharybriggs64@gmail.com>
2018-06-04 17:57:11 -0500

    enemy now chases player

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehaviour : MonoBehaviour {
-
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 boom;
+    public float Speed;
+    private CharacterController controller;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    controller = gameObject.GetComponent<CharacterController>();
+        boom = this.transform.position - target.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+	{
+	    Vector3 move_direction = new Vector3(0,0,0);
+        transform.LookAt(target.position);
+	    move_direction = transform.forward * Speed;
+	    controller.Move(move_direction * Time.deltaTime);
+    }
 }

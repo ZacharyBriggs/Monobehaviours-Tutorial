c4475cb4204df1e4ecfd1798d8578c1418327d72
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-06-05 17:55:36 -0500

    changed enemy behaviour and added spawners and spawner behaviour

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    public Transform target;
+    private PlayerMovementBehaviour player;
     private Vector3 boom;
     public float Speed;
     private CharacterController controller;
 	// Use this for initialization
 	void Start ()
 	{
-	    controller = gameObject.GetComponent<CharacterController>();
-        boom = this.transform.position - target.position;
+	    player = GameObject.FindObjectOfType<PlayerMovementBehaviour>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    Vector3 move_direction = new Vector3(0,0,0);
-        transform.LookAt(target.position);
-	    move_direction = transform.forward * Speed;
-	    controller.Move(move_direction * Time.deltaTime);
-    }
+	    Vector3 dirToPlayer;
+	    dirToPlayer = player.transform.position - this.transform.position;
+        dirToPlayer.Normalize();
+	    transform.position += dirToPlayer * Speed * Time.deltaTime;
+	}
 }
---
+++ Assets/Scripts/SpawnerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehaviour : MonoBehaviour
+{
+
+    public GameObject enemy_prefab;
+    public float spawn_time;
+    public float spawn_radius;
+    private PlayerMovementBehaviour player;
+    private float spawn_timer;
+	void Start ()
+	{
+	    spawn_timer = spawn_time;
+	    player = GameObject.FindObjectOfType<PlayerMovementBehaviour>();
+	}
+	
+	void Update ()
+	{
+	    spawn_timer -= Time.deltaTime;
+	    if (spawn_timer < 0)
+	    {
+	        spawn_timer = spawn_time;
+	        float spawn_angle = Random.Range(0, 2 * Mathf.PI);
+            Vector3 spawn_direction = new Vector3(Mathf.Sin(spawn_angle), 0, Mathf.Cos(spawn_angle));
+	        spawn_direction *= spawn_radius;
+	        Vector3 spawn_point = player.transform.position + spawn_direction;
+	        Instantiate(enemy_prefab, spawn_point, Quaternion.identity);
+	    }
+	}
+}
